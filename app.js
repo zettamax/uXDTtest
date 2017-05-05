d66e77a997ab0e0ae9213384b5d6c7aae5c2b767
Max Nazarov <zettamax@gmail.com>
2017-05-05 14:36:23 +0300

    Fix playing beacons more than one time

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 var context = new window.AudioContext();
 var osc = context.createOscillator();
+osc.type = 'sine';
+osc.frequency.value = 15000;
+osc.start();
 
 window.onload = function () {
 
     document.querySelector('#play').onclick = function () {
-        osc.type = 'sine';
-        osc.frequency.value = 15000;
         osc.connect(context.destination);
-        osc.start();
-        osc.stop(context.currentTime + 2);
+        setTimeout(function () {
+            osc.disconnect(context.destination);
+        }, 1000);
     };
 
     document.querySelector('#listen').onclick = function () {
