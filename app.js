4b6f53b921728f27ea134ba94f5ca4459e79881c
Max Nazarov <zettamax@gmail.com>
2017-05-05 14:41:03 +0300

    Fix play button double clicking

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var context = new window.AudioContext();
 var osc = context.createOscillator();
+var playing = false;
 osc.type = 'sine';
 osc.frequency.value = 15000;
 osc.start();
@@ -7,10 +8,14 @@ osc.start();
 window.onload = function () {
 
     document.querySelector('#play').onclick = function () {
-        osc.connect(context.destination);
-        setTimeout(function () {
-            osc.disconnect(context.destination);
-        }, 1000);
+        if (!playing) {
+            osc.connect(context.destination);
+            playing = true;
+            setTimeout(function () {
+                osc.disconnect(context.destination);
+                playing = false;
+            }, 1000);
+        }
     };
 
     document.querySelector('#listen').onclick = function () {
