e0d256bb216f6cef4b393b8645dce9418fa2ca56
Max Nazarov <zettamax@gmail.com>
2017-05-05 14:30:25 +0300

    Change frequency for testing

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ window.onload = function () {
 
     document.querySelector('#play').onclick = function () {
         osc.type = 'sine';
-        osc.frequency.value = 18000;
+        osc.frequency.value = 15000;
         osc.connect(context.destination);
         osc.start();
         osc.stop(context.currentTime + 2);
