990858890122c3072aa9be87e075b41606104e40
Max Nazarov <zettamax@gmail.com>
2017-05-05 14:43:57 +0300

    Use disabled button attr instead of global var

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var context = new window.AudioContext();
 var osc = context.createOscillator();
-var playing = false;
 osc.type = 'sine';
 osc.frequency.value = 15000;
 osc.start();
@@ -8,12 +7,13 @@ osc.start();
 window.onload = function () {
 
     document.querySelector('#play').onclick = function () {
-        if (!playing) {
+        var button = this;
+        if (!button.disabled) {
             osc.connect(context.destination);
-            playing = true;
+            button.disabled = true;
             setTimeout(function () {
                 osc.disconnect(context.destination);
-                playing = false;
+                button.disabled = false;
             }, 1000);
         }
     };
