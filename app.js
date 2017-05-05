f3ba5679787f151a4cbbfade6c00884002a9b78c
Max Nazarov <zettamax@gmail.com>
2017-05-05 14:28:57 +0300

    Add index file; Add script generating beacons

---
+++ app.js
@@ -0,0 +1,18 @@
+var context = new window.AudioContext();
+var osc = context.createOscillator();
+
+window.onload = function () {
+
+    document.querySelector('#play').onclick = function () {
+        osc.type = 'sine';
+        osc.frequency.value = 18000;
+        osc.connect(context.destination);
+        osc.start();
+        osc.stop(context.currentTime + 2);
+    };
+
+    document.querySelector('#listen').onclick = function () {
+        alert('Not implemented');
+    };
+
+};
