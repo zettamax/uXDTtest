30a0132be95899fac4d4cc9afa93214b5490bd49
Max Nazarov <zettamax@gmail.com>
2017-05-06 22:40:09 +0300

    Change fft size

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ function analyzeStream(stream) {
 
     micSource.connect(filter);
 
-    analyser.fftSize = 256;
+    analyser.fftSize = 2048;
     micSource.connect(analyser);
 
     var WIDTH = canvas.width;
